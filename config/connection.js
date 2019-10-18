22c5478f90a1bab786a47594ef626acf893aa8d2
Fauz Ahmed <fauz.ahmed369@gmail.com>
2019-10-18 13:04:27 -0500

    password change test

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection(process.env.JAWSDB_URL||{
   host: "localhost",
   port: 3306,
   user: "root",
-  password: process.env.DBPassword,
+  password: 'feriner147',
   database: "burgers_db"
 });
 
