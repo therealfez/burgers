3831e7fd751f4b083b63e5c04be374b80c972366
Fauz Ahmed <fauz.ahmed369@gmail.com>
2019-10-17 19:07:46 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom and Swiss');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Monster');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Ultimate Double Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken Sandwich');
+
+SELECT * FROM burgers;
